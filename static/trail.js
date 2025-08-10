fa76378331ae6a8492be0269c91ae3ecd93db3e4
Lamampis <lampis750@tutanota.com>
2025-08-10 17:45:37 +0300

    trail improvement again

--- static/trail.js
+++ static/trail.js
@@ -138,7 +138,7 @@ function sparkle() {
     if (starv[c]) updateStar(c);
     if (tinyv[c]) updateTiny(c);
   }
-  setTimeout(sparkle, 19); // Speed
+  setTimeout(sparkle, 16); // Speed
 }
 
 function updateStar(i) {
